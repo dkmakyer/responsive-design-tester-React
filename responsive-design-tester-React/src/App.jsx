61131f7da5ddfca8f8855b382b31ad741f452ddd
dkmakyer <kwasiakyer2105@gmail.com>
2024-10-15 08:49:00 +0000

    updated App.css
    made the appearance make sense

--- responsive-design-tester-React/src/App.jsx
+++ responsive-design-tester-React/src/App.jsx
@@ -1,56 +1,57 @@
 import React from 'react';
+import "./App.css"
 import { faTabletScreenButton, faDesktop, faMobile } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"; // Correct React FontAwesome package
 
 const App = () => {
-  const [defaultHeight, setDefaultHeight] = React.useState(null); 
+  const [defaultHeight, setDefaultHeight] = React.useState(null);
   const [defaultWidth, setDefaultWidth] = React.useState(null);
   const [inputUrl, setInputUrl] = React.useState('');
   const [storedUrl, setStoredUrl] = React.useState('');
-  
+
   const [clicked, setClicked] = React.useState({
     mobile: false,
     desktop: false,
     tablet: false,
   });
-  
+
   let mobile = [
-      { "Apple iPhone 3/4/4s": { width: 320, height: 480 } },
-      { "Apple iPhone 5/5s": { width: 320, height: 568 } },
-      { "Apple iPhone 6/6s/7": { width: 375, height: 667 } },
-      { "Apple iPhone 6s Plus / 7 Plus": { width: 414, height: 736 } },
-      { "Samsung Galaxy S5/S6/S7": { width: 360, height: 640 } },
-      { "Sony Xperia Z2/Z3": { width: 360, height: 640 } },
-      { "Google Pixel": { width: 411, height: 731 } },
-      { "Nexus 4": { width: 384, height: 640 } },
-      { "Samsung Galaxy S8": { width: 360, height: 740 } },
-      { "OnePlus 8": { width: 412, height: 869 } },
+    { "Apple iPhone 3/4/4s": { width: 320, height: 480 } },
+    { "Apple iPhone 5/5s": { width: 320, height: 568 } },
+    { "Apple iPhone 6/6s/7": { width: 375, height: 667 } },
+    { "Apple iPhone 6s Plus / 7 Plus": { width: 414, height: 736 } },
+    { "Samsung Galaxy S5/S6/S7": { width: 360, height: 640 } },
+    { "Sony Xperia Z2/Z3": { width: 360, height: 640 } },
+    { "Google Pixel": { width: 411, height: 731 } },
+    { "Nexus 4": { width: 384, height: 640 } },
+    { "Samsung Galaxy S8": { width: 360, height: 740 } },
+    { "OnePlus 8": { width: 412, height: 869 } },
   ];
 
   let desktop = [
-      { "24_desktop": { width: 1920, height: 1200 } },
-      { "23_desktop": { width: 1920, height: 1080 } },
-      { "22_desktop": { width: 1680, height: 1050 } },
-      { "20_desktop": { width: 1600, height: 900 } },
-      { "19_desktop": { width: 1440, height: 900 } },
-      { "15_notebook": { width: 1366, height: 768 } },
-      { "13_notebook": { width: 1024, height: 800 } },
-      { "10_notebook": { width: 1024, height: 600 } },
-      { "Apple Thunderbolt Display": { width: 2560, height: 1440 } },
-      { "Dell UltraSharp Monitor": { width: 1440, height: 900 } },
+    { "24_desktop": { width: 1920, height: 1200 } },
+    { "23_desktop": { width: 1920, height: 1080 } },
+    { "22_desktop": { width: 1680, height: 1050 } },
+    { "20_desktop": { width: 1600, height: 900 } },
+    { "19_desktop": { width: 1440, height: 900 } },
+    { "15_notebook": { width: 1366, height: 768 } },
+    { "13_notebook": { width: 1024, height: 800 } },
+    { "10_notebook": { width: 1024, height: 600 } },
+    { "Apple Thunderbolt Display": { width: 2560, height: 1440 } },
+    { "Dell UltraSharp Monitor": { width: 1440, height: 900 } },
   ];
 
   let tablet = [
-      { "Apple iPad Mini": { width: 768, height: 1024 } },
-      { "Apple iPad Retina": { width: 768, height: 1024 } },
-      { "Apple iPad Pro": { width: 1366, height: 1024 } },
-      { "Amazon Kindle Fire": { width: 768, height: 1024 } },
-      { "Amazon Kindle Fire HD": { width: 768, height: 1024 } },
-      { "Asus Eee 1000": { width: 768, height: 1024 } },
-      { "Nexus 7": { width: 600, height: 960 } },
-      { "Nexus 9": { width: 1024, height: 768 } },
-      { "Samsung Galaxy Tab S7": { width: 2560, height: 1600 } },
-      { "Microsoft Surface Pro": { width: 2736, height: 1824 } },
+    { "Apple iPad Mini": { width: 768, height: 1024 } },
+    { "Apple iPad Retina": { width: 768, height: 1024 } },
+    { "Apple iPad Pro": { width: 1366, height: 1024 } },
+    { "Amazon Kindle Fire": { width: 768, height: 1024 } },
+    { "Amazon Kindle Fire HD": { width: 768, height: 1024 } },
+    { "Asus Eee 1000": { width: 768, height: 1024 } },
+    { "Nexus 7": { width: 600, height: 960 } },
+    { "Nexus 9": { width: 1024, height: 768 } },
+    { "Samsung Galaxy Tab S7": { width: 2560, height: 1600 } },
+    { "Microsoft Surface Pro": { width: 2736, height: 1824 } },
   ];
 
   function toggleSidebar(deviceType) {
@@ -63,15 +64,15 @@ const App = () => {
   }
 
   function dropDownPage(inputUrl) {
-      setDefaultHeight(1920);
-      setDefaultWidth(1080);
-      setStoredUrl(inputUrl);
+    setDefaultHeight(1920);
+    setDefaultWidth(1080);
+    setStoredUrl(inputUrl);
   }
 
   function selectDevice(width, height) {
-      setDefaultWidth(width);
-      setDefaultHeight(height);
-      setClicked({ mobile: false, desktop: false, tablet: false });
+    setDefaultWidth(width);
+    setDefaultHeight(height);
+    setClicked({ mobile: false, desktop: false, tablet: false });
   }
 
   const renderDeviceList = (deviceType) => {
@@ -95,7 +96,7 @@ const App = () => {
   return (
     <>
       <div className="container mx-auto max-w-screen-xl flex flex-col lg:flex-row overflow-hidden rounded-lg">
-        <div className="side bg-gray-400 w-40 shadow-xl min-h-screen top-0 left-0 bottom-0">
+        <div className="side bg-gray-400 fixed rounded-none  w-40 shadow-xl min-h-screen top-0 left-0 bottom-0">
           <button
             className="flex flex-col justify-between items-center"
             onClick={() => toggleSidebar("mobile")}
@@ -119,7 +120,7 @@ const App = () => {
         </div>
 
         <main className="flex-1 py-16 w-screen">
-          <div className="input-area mt-16 sticky flex flex-row items-center">
+          <div className="input-area fixed mt-4 flex flex-row items-center">
             <input
               type="text"
               value={inputUrl}
@@ -133,14 +134,14 @@ const App = () => {
             >Go</button>
           </div>
           <div className="content mt-10 overflow-hidden max-h-auto">
-            <object
-              type="text/html"
-              data={storedUrl}
+            <iframe
+              src={storedUrl}
               width={defaultWidth}
               height={defaultHeight}
               title="drop-down-page"
-            >
-            </object>
+            style={{ border: "none" }}
+            />
+
           </div>
           <div className="mini-sidebar">
             {clicked.mobile && <div className="submenu"><ul>{renderDeviceList("mobile")}</ul></div>}
